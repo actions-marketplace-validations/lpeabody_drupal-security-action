3d88196fbcd1df33c3c8a78f19a59fb95f2a24a8
Les Peabody <lpeabody@gmail.com>
2021-01-08 18:59:44 -0500

    Specify latest tag.

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm \
-    -v $PWD/composer.json:/app/composer.json \
-    -v $PWD/composer.lock:/app/composer.lock \
-    lpeabody/drupal-security drush pm:security
